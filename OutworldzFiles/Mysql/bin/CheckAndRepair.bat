e141f65d53acd66f3f397a81c42dd148acc46664
Fred Beckhusen <fred@mitsi.com>
2019-03-22 13:36:57 -0500

    Optional Mysql port

--- OutworldzFiles/Mysql/bin/CheckAndRepair.bat
+++ OutworldzFiles/Mysql/bin/CheckAndRepair.bat
@@ -1,15 +1,19 @@
 @echo off
-echo Upgrading MySql Database
-mysql_upgrade.exe --port=%1
+echo Upgrading MySql Database. Useage:  CheckAndRepair [Port number]
+SET PORT = %1
+IF "%PORT%" == "" (SET PORT=3306)
+mysql_upgrade.exe --port=%PORT%
 
 myisamchk --force --fast --update-state ..\data\mysql\*.MYI
 myisamchk --force --fast --update-state ..\data\robust\*.MYI
 
 echo Checking Database
-mysqlcheck.exe --port %1 -u root -A 
+mysqlcheck.exe --port %PORT% -u root -A 
 set /p fixmysql=Repair and Optimize [y/n]?:
-IF "%fixmysql%" == "y" mysqlcheck.exe --port %1 -A  -u root -r
-IF "%fixmysql%" == "y" mysqlcheck.exe --port %1 -A  -u root -o
+@echo Repairing, please wait!
+IF "%fixmysql%" == "y" mysqlcheck.exe --port %PORT% -A  -u root -r
+@echo Optimizing, please wait!
+IF "%fixmysql%" == "y" mysqlcheck.exe --port %PORT% -A  -u root -o
 
 set /p temp="Press enter to exit"
 
--- OutworldzFiles/Mysql/bin/Repair_ISAM.bat
+++ OutworldzFiles/Mysql/bin/Repair_ISAM.bat
@@ -1,5 +1,4 @@
+@remarkable program to repair Mysql when it will not start
 myisamchk --force --fast --update-state ..\data\mysql\*.MYI
-myisamchk --force --fast --update-state ..\data\opensim\*.MYI
 myisamchk --force --fast --update-state ..\data\robust\*.MYI
-
-pause
+@pause
