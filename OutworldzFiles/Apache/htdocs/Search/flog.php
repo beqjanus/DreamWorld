f52a7cc196bb5452cfbd45ce64ac49888d75ffdb
Fred Beckhusen <fred@mitsi.com>
2019-04-14 22:21:24 -0500

    typo

--- OutworldzFiles/Apache/htdocs/Search/flog.php
+++ OutworldzFiles/Apache/htdocs/Search/flog.php
@@ -33,7 +33,9 @@
  *  Email:  iliep@softconsultant.ro
  *  Date:   26-Nov-2011
  */
- 
+	
+define ("FLOG_IGNORE", true);	// comment this to get data in logs
+
 if( defined( "FLOG_HAS_BEEN_INCLUDED" ) ) return;
 define( "FLOG_HAS_BEEN_INCLUDED", true );
 
