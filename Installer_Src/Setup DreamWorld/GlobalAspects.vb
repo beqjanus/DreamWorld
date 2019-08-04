51f1a3c4322a07664dce260b67c55d20765e07ee
Fred Beckhusen <fred@mitsi.com>
2019-08-03 19:58:33 -0500

    Add PostSharp for threading management warning

---
+++ Installer_Src/Setup DreamWorld/GlobalAspects.vb
@@ -0,0 +1,4 @@
+﻿Imports PostSharp.Patterns.Threading
+' This file contains registration of aspects that are applied to several classes of this project.
+<Assembly: DeadlockDetectionPolicy>
+<Assembly: ThreadSafetyPolicy>
