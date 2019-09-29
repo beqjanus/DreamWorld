0640244e6b7b47585d136147ff9dd3899daae8d4
Fred Beckhusen <fred@mitsi.com>
2019-09-29 14:09:28 -0500

    new Module

---
+++ Installer_Src/Setup DreamWorld/RandomNumber.vb
@@ -0,0 +1,8 @@
+﻿Module RandomNumber
+
+    Public Function Random() As String
+        Dim value As Integer = CInt(Int((600000000 * Rnd()) + 1))
+        Random = System.Convert.ToString(value, Form1.Invarient)
+    End Function
+
+End Module
