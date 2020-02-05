8813aed801e757e5836f449871a086226c0fd92b
Fred Beckhusen <fred@mitsi.com>
2020-02-05 13:55:12 -0600

    V3.35 Start

--- Installer_Src/Setup DreamWorld/FormSetup.vb
+++ Installer_Src/Setup DreamWorld/FormSetup.vb
@@ -37,7 +37,7 @@ Imports MySql.Data.MySqlClient
 Public Class Form1
 
 #Region "Version"
-    Private _MyVersion As String = "3.34"
+    Private _MyVersion As String = "3.35"
     Private _SimVersion As String = "066a6fbaa1 (changes on lludp acks and resends, 2019-12-18)"
 #End Region
 
