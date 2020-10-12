50185abb78411b41d620ce1bc8950df358d77c8e
Fred Beckhusen <fred@mitsi.com>
2020-10-11 21:18:31 -0500

    V7.1 Gloebits fix

--- OutworldzFiles/PHP7a/databaseinfo.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$DB_GRIDNAME = "192.168.2.133:8002";
-$DB_HOST = "127.0.0.1";
-$DB_PORT = "3306"; // Robust port 
-$DB_USER = "robustuser";
-$DB_PASSWORD = "robustpassword";
-$DB_NAME = "ossearch";
-?>
