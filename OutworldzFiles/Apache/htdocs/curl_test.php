ff40e1530e91fc1660ffeab2d5545db1d65e2b66
Fred Beckhusen <fred@outworldz.com>
2020-11-11 22:13:25 -0600

    curl_test.php fetches IP from api.ipify.org

--- OutworldzFiles/Apache/htdocs/curl_test.php
+++ OutworldzFiles/Apache/htdocs/curl_test.php
@@ -2,9 +2,10 @@
     error_reporting(E_ALL);
     ini_set('display_errors', '1');
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'http://127.0.0.1/');
+    curl_setopt($ch, CURLOPT_URL, 'http://api.ipify.org/');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $contents = curl_exec ($ch);
+    echo "Public IP as fetched from apify.org is ";
     echo $contents;
     curl_close ($ch);
 ?>
