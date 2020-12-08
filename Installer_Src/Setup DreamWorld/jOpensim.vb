ab61dfc5ddbd63e19d86a4164ff6f6786f70c4a9
Fred Beckhusen <fred@outworldz.com>
2020-12-08 12:07:56 -0600

    Add Joomla module If no Joomla, revert to reasonable CMS

---
+++ Installer_Src/Setup DreamWorld/jOpensim.vb
@@ -0,0 +1,28 @@
+﻿Module Joomla
+
+    Public Function IsjOpensimInstalled() As Boolean
+
+        Dim count As Integer
+        Try
+            Dim folders() = IO.Directory.GetFiles(IO.Path.Combine(Settings.CurrentDirectory, "Outworldzfiles\Apache\htdocs\JOpensim"))
+            count = folders.Length
+        Catch
+        End Try
+
+        Return count > 1
+
+    End Function
+
+    Public Sub CheckForjOpensimUpdate()
+
+        Dim installed As Boolean = IsjOpensimInstalled()
+
+        Dim file = IO.Path.Combine(Settings.CurrentDirectory, "Outworldzfiles\Apache\htdocs\jOpensim\" & FormSetup.JOpensimRev1)
+
+        If Not IO.File.Exists(file) And installed Then
+            HelpManual("Joomla Update")
+        End If
+
+    End Sub
+
+End Module
