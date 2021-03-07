ebaa803efc1ae457f1d21423a6ab85097ec9003b
Fred Beckhusen <fred@outworldz.com>
2021-03-07 01:49:48 -0600

    Force scripts to compile on 1st on boot for co-op change.

--- Installer_Src/Setup DreamWorld/GlobalSettings.vb
+++ Installer_Src/Setup DreamWorld/GlobalSettings.vb
@@ -8,7 +8,7 @@ Module GlobalSettings
 
     Public Const _Domain As String = "http://outworldz.com"
     Public Const _MyVersion As String = "3.899"
-    Public Const _SimVersion As String = "#be49d426d9=>1610c3f741 (mantis 8862: do cancel negative cache on valid store"
+    Public Const _SimVersion As String = "#be49d426d9=>1610c3f741 (mantis 8862: do cancel negative cache on valid store."
     Public Const jOpensimRev As String = "Joomla_3.9.23-Stable-Full_Package"
     Public Const jRev As String = "3.9.23"
     Public Const MySqlRev = "5.6.5"
