646f719ca90abd2012d2a79583853e2fc1b11e88
Fred Beckhusen <fred@outworldz.com>
2021-03-08 06:56:59 -0600

    SS form

---
+++ Installer_Src/Setup DreamWorld/FormSmartStart.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormSmartStart
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormSmartStart))
+        Me.SmartStartEnabled = New System.Windows.Forms.CheckBox()
+        Me.Seconds = New System.Windows.Forms.TextBox()
+        Me.DelayLabel = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SmartStartEnabled
+        '
+        Me.SmartStartEnabled.AutoSize = True
+        Me.SmartStartEnabled.Location = New System.Drawing.Point(39, 38)
+        Me.SmartStartEnabled.Margin = New System.Windows.Forms.Padding(1)
+        Me.SmartStartEnabled.Name = "SmartStartEnabled"
+        Me.SmartStartEnabled.Size = New System.Drawing.Size(114, 17)
+        Me.SmartStartEnabled.TabIndex = 12
+        Me.SmartStartEnabled.Text = Global.Outworldz.My.Resources.Resources.Smart_Start_Enable_word
+        Me.SmartStartEnabled.UseVisualStyleBackColor = True
+        '
+        'Seconds
+        '
+        Me.Seconds.Location = New System.Drawing.Point(39, 74)
+        Me.Seconds.Margin = New System.Windows.Forms.Padding(1)
+        Me.Seconds.Name = "Seconds"
+        Me.Seconds.Size = New System.Drawing.Size(40, 20)
+        Me.Seconds.TabIndex = 13
+        '
+        'DelayLabel
+        '
+        Me.DelayLabel.AutoSize = True
+        Me.DelayLabel.Location = New System.Drawing.Point(83, 74)
+        Me.DelayLabel.Name = "DelayLabel"
+        Me.DelayLabel.Size = New System.Drawing.Size(39, 13)
+        Me.DelayLabel.TabIndex = 14
+        Me.DelayLabel.Text = "Label1"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.SmartStartEnabled)
+        Me.GroupBox1.Controls.Add(Me.DelayLabel)
+        Me.GroupBox1.Controls.Add(Me.Seconds)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 47)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(257, 144)
+        Me.GroupBox1.TabIndex = 15
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(286, 24)
+        Me.MenuStrip1.TabIndex = 16
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.Image = Global.Outworldz.My.Resources.Resources.about
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'FormSmartStart
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(286, 203)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "FormSmartStart"
+        Me.Text = "Smart Start"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SmartStartEnabled As CheckBox
+    Friend WithEvents Seconds As TextBox
+    Friend WithEvents DelayLabel As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Installer_Src/Setup DreamWorld/FormSmartStart.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class FormSmartStart
+
+#Region "ScreenSize"
+
+    Private ReadOnly Handler As New EventHandler(AddressOf Resize_page)
+
+    'The following detects  the location of the form in screen coordinates
+    Private _screenPosition As ScreenPos
+
+    Public Property ScreenPosition As ScreenPos
+        Get
+            Return _screenPosition
+        End Get
+        Set(value As ScreenPos)
+            _screenPosition = value
+        End Set
+    End Property
+
+    Private Sub Resize_page(ByVal sender As Object, ByVal e As System.EventArgs)
+        'Me.Text = "Form screen position = " + Me.Location.ToString
+        ScreenPosition.SaveXY(Me.Left, Me.Top)
+    End Sub
+
+    Private Sub SetScreen()
+        Me.Show()
+        ScreenPosition = New ScreenPos(Me.Name)
+        AddHandler ResizeEnd, Handler
+        Dim xy As List(Of Integer) = ScreenPosition.GetXY()
+        Me.Left = xy.Item(0)
+        Me.Top = xy.Item(1)
+    End Sub
+
+#End Region
+
+    Private Sub HelpToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HelpToolStripMenuItem.Click
+
+        HelpManual("SmartStart")
+
+    End Sub
+
+    Private Sub Loaded(sender As Object, e As EventArgs) Handles Me.Load
+
+        SmartStartEnabled.Text = Global.Outworldz.My.Resources.Smart_Start_Enable_word
+        DelayLabel.Text = Global.Outworldz.My.Resources.SSDelay
+        Me.Text = Global.Outworldz.My.Resources.Smart_Start_word
+        GroupBox1.Text = Global.Outworldz.My.Resources.Smart_Start_Enable_word
+        SmartStartEnabled.Checked = Settings.SmartStart
+        Seconds.Text = CStr(Settings.SmartStartTimeout)
+        SetScreen()
+
+    End Sub
+
+    Private Sub Seconds_TextChanged(sender As Object, e As EventArgs) Handles Seconds.TextChanged
+        Dim digitsOnly As Regex = New Regex("[^\d]")
+        Seconds.Text = digitsOnly.Replace(Seconds.Text, "")
+        Settings.SmartStartTimeout = CInt("0" & Seconds.Text)
+    End Sub
+
+    Private Sub SmartStartEnabled_CheckedChanged(sender As Object, e As EventArgs) Handles SmartStartEnabled.CheckedChanged
+        Settings.SmartStart = SmartStartEnabled.Checked
+    End Sub
+
+End Class
