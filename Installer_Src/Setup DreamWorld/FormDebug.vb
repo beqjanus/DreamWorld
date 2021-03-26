1d47e9bf256e79a03020ced33fe90e414fa34c9c
Fred Beckhusen <fred@outworldz.com>
2021-03-26 11:05:23 +0100

    Set Sreen coords for debug menu

--- Installer_Src/Setup DreamWorld/FormDebug.vb
+++ Installer_Src/Setup DreamWorld/FormDebug.vb
@@ -7,6 +7,38 @@ Public Class FormDebug
     Private _command As String
     Private _value As Boolean
 
+
+#Region "FormPos"
+
+    Private ReadOnly Handler As New EventHandler(AddressOf Resize_page)
+    Private _screenPosition As ScreenPos
+
+    Public Property ScreenPosition As ScreenPos
+        Get
+            Return _screenPosition
+        End Get
+        Set(value As ScreenPos)
+            _screenPosition = value
+        End Set
+    End Property
+
+    'The following detects  the location of the form in screen coordinates
+    Private Sub Resize_page(ByVal sender As Object, ByVal e As System.EventArgs)
+        'Me.Text = "Form screen position = " + Me.Location.ToString
+        ScreenPosition.SaveXY(Me.Left, Me.Top)
+    End Sub
+
+    Private Sub SetScreen()
+        Me.Show()
+        ScreenPosition = New ScreenPos(Me.Name)
+        AddHandler ResizeEnd, Handler
+        Dim xy As List(Of Integer) = ScreenPosition.GetXY()
+        Me.Left = xy.Item(0)
+        Me.Top = xy.Item(1)
+    End Sub
+
+#End Region
+
 #Region "Properties"
 
     Public Property Abort As Boolean
@@ -56,6 +88,15 @@ Public Class FormDebug
 
 #End Region
 
+#Region "Scrolling text box"
+
+    Private Sub TextBox1_Changed(sender As System.Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim ln As Integer = TextBox1.Text.Length
+        TextBox1.SelectionStart = ln
+        TextBox1.ScrollToCaret()
+    End Sub
+
+#End Region
 #Region "Start/Stop"
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
@@ -163,9 +204,14 @@ Public Class FormDebug
                     Dim p = IO.Path.Combine(Settings.OpensimBinPath, $"Regions\{shortname}\Region\{shortname}.ini")
 
                     If Not IO.File.Exists(p) Then
+                        ProgressPrint($"{My.Resources.Add_Region_word} {J.Name} ")
                         RegionUUID = PropRegionClass.CreateRegion(shortname)
                     Else
                         RegionUUID = PropRegionClass.FindRegionByName(shortname)
+                        If GetPrimCount(RegionUUID) > 0 Then
+                            ProgressPrint($"{J.Name} {My.Resources.Ok} ")
+                            Continue For
+                        End If
                     End If
 
                     Dim g = New Guid
@@ -204,7 +250,6 @@ Public Class FormDebug
                     PropRegionClass.RegionPort(RegionUUID) = port
                     PropRegionClass.WriteRegionObject(shortname)
 
-                    ProgressPrint($"{My.Resources.Add_Region_word} {J.Name} @ {CStr(X)},{CStr(Y)}")
                     PropUpdateView = True ' make form refresh
                     Application.DoEvents()
 
@@ -241,7 +286,6 @@ Public Class FormDebug
                         PropRegionClass.Status(RegionUUID) = RegionMaker.SIMSTATUSENUM.NoError
 
                         If EstateName(RegionUUID).Length = 0 Then
-                            ConsoleCommand(RegionUUID, "{ENTER}")
                             ConsoleCommand(RegionUUID, Estate)
                         End If
 
@@ -365,13 +409,18 @@ Public Class FormDebug
 
     Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
 
+        If Debugger.IsAttached Then
+            ComboBox1.Items.Add(My.Resources.SmartStartEnable)
+        End If
+
         ComboBox1.Items.Add(My.Resources.LoadFreeOars)
-        ComboBox1.Items.Add(My.Resources.SmartStartEnable)
         ComboBox1.Items.Add($"{My.Resources.Debug_word} Off")
         ComboBox1.Items.Add($"{My.Resources.Debug_word} 1 Minute")
         ComboBox1.Items.Add($"{My.Resources.Debug_word} 10 Minutes")
         ComboBox1.Items.Add($"{My.Resources.Debug_word} 60 Minutes")
         ComboBox1.Items.Add($"{My.Resources.Debug_word} 24 hours")
+
+        SetScreen()
 
     End Sub
 
