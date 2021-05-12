0edc6f6ce5e0d8e6d8182aa0717467872dced8ce
Fred Beckhusen <fred@outworldz.com>
2021-05-11 22:51:09 -0500

    Please Wait msg

--- Installer_Src/Setup DreamWorld/My Project/Resources.Designer.vb
+++ Installer_Src/Setup DreamWorld/My Project/Resources.Designer.vb
@@ -7088,6 +7088,15 @@ Namespace My.Resources
         End Property
         
         '''<summary>
+        '''  Looks up a localized string similar to Please wait for Robust to completely boot. This may take some minutes..
+        '''</summary>
+        Friend ReadOnly Property PleaseWait() As String
+            Get
+                Return ResourceManager.GetString("PleaseWait", resourceCulture)
+            End Get
+        End Property
+        
+        '''<summary>
         '''  Looks up a localized string similar to Polish.
         '''</summary>
         Friend ReadOnly Property Polish() As String
