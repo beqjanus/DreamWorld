25d951430623a784bebdbbb5adfe999a41a932be
Fred Beckhusen <fred.b@mitsi.com>
2021-05-22 23:53:05 -0500

    1st5 try at a Region HTTP port probe. ?version does not work. a read of the HTTP port/index.php is a redirect to Opensimulator.org, which is okay, but the region is not necessarily running

--- Installer_Src/Setup DreamWorld/Teleport.vb
+++ Installer_Src/Setup DreamWorld/Teleport.vb
@@ -5,6 +5,8 @@
 
 #End Region
 
+Imports System.Net
+
 Module Teleport
 
     Public Fin As New List(Of String)
@@ -18,18 +20,17 @@ Module Teleport
                 Dim AgentID = Keypair.Key
                 Dim RegionToUUID = Keypair.Value
                 Dim status = PropRegionClass.Status(RegionToUUID)
+                Dim Port As Integer = PropRegionClass.GroupPort(RegionToUUID)
                 Dim DestinationName = PropRegionClass.RegionName(RegionToUUID)
                 If status = RegionMaker.SIMSTATUSENUM.Booting Then
                     PokeRegionTimer(RegionToUUID)
                 End If
-                If status = RegionMaker.SIMSTATUSENUM.Booted Then
-
-                    Debug.Print($"Teleport to {DestinationName} = {GetStateString(status)}")
+                If status = RegionMaker.SIMSTATUSENUM.Booted And IsRegionReady(Port) Then
                     Dim FromRegionUUID As String = GetRegionFromAgentID(AgentID)
                     Dim fromName = PropRegionClass.RegionName(FromRegionUUID)
-                    Logger("Teleport", $"Teleport from {fromName} to {DestinationName} initiated", "Teleport")
                     If fromName.Length > 0 Then
 
+                        Logger("Teleport", $"Teleport from {fromName} to {DestinationName} initiated", "Teleport")
                         ' Double Check region
                         If RegionIsRegisteredOnline(RegionToUUID) Then
                             If TeleportTo(FromRegionUUID, DestinationName, AgentID) Then
@@ -62,5 +63,32 @@ Module Teleport
         Fin.Clear()
 
     End Sub
+
+
+    ''' <summary>Check if region is ready for l;ogin</summary>
+    ''' <returns>true if up</returns>
+    Public Function IsRegionReady(Port As Integer) As Boolean
+
+        Using client As New WebClient ' download client for web pages
+            Dim Up As String
+            Try
+                Up = client.DownloadString($"http://{Settings.PublicIP}:{CStr(Port)}/index.php")
+            Catch ex As Exception
+                ' If ex.Message.Contains("404") Then
+                '   RobustIcon(True)
+                '   Log("INFO", "Robust is running")
+                '   Return True
+                ' End If
+                Return False
+            End Try
+
+            If Up.Length = 0 And PropOpensimIsRunning() Then
+                Return False
+            End If
+        End Using
+
+        Return True
+
+    End Function
 
 End Module
