2f5908304caaa1a691e5484453e2de95d4313de3
Fred Beckhusen <fred@outworldz.com>
2021-07-31 16:10:22 -0500

    Events as a sewperate thread

--- Installer_Src/Setup DreamWorld/Events.vb
+++ Installer_Src/Setup DreamWorld/Events.vb
@@ -48,57 +48,59 @@ Module Events
                 stm += " values (@simname,@category,@creatoruuid,@owneruuid,@name,@description,@dateUTC,@duration,@covercharge,@coveramount,@parcelUUID,@globalPos,@gateway,@eventflags)"
 
                 Dim Simevent As New Dictionary(Of String, String)
-                Using client As New WebClient()
-                    Dim Stream = client.OpenRead(PropDomain() & "/events.txt?r=" & RandomNumber.Random)
-                    Using reader = New StreamReader(Stream)
-                        Application.DoEvents()
+                Try
+                    Using client As New WebClient()
+                        Dim Stream = client.OpenRead(PropDomain() & "/events.txt?r=" & RandomNumber.Random)
+                        Using reader = New StreamReader(Stream)
+                            Application.DoEvents()
 
-                        While reader.Peek <> -1
-                            Dim s = reader.ReadLine
-                            ' Split line on comma.
-                            Dim array As String() = s.Split("|".ToCharArray())
-                            Simevent.Clear()
-                            ' Loop over each string received.
-                            Dim part As String
-                            For Each part In array
-                                ' Display to console.
-                                Dim a As String() = part.Split("^".ToCharArray())
-                                If a.Length = 2 Then
-                                    a(1) = a(1).Replace("'", "\'")
-                                    a(1) = a(1).Replace("`", vbLf)
-                                    'Diagnostics.Debug.Print("{0}:{1}", a(0), a(1))
-                                    If Not Simevent.ContainsKey(a(0)) Then
-                                        Simevent.Add(a(0), a(1))
+                            While reader.Peek <> -1
+                                Dim s = reader.ReadLine
+                                ' Split line on comma.
+                                Dim array As String() = s.Split("|".ToCharArray())
+                                Simevent.Clear()
+                                ' Loop over each string received.
+                                Dim part As String
+                                For Each part In array
+                                    ' Display to console.
+                                    Dim a As String() = part.Split("^".ToCharArray())
+                                    If a.Length = 2 Then
+                                        a(1) = a(1).Replace("'", "\'")
+                                        a(1) = a(1).Replace("`", vbLf)
+                                        'Diagnostics.Debug.Print("{0}:{1}", a(0), a(1))
+                                        If Not Simevent.ContainsKey(a(0)) Then
+                                            Simevent.Add(a(0), a(1))
+                                        End If
                                     End If
-                                End If
-                            Next
+                                Next
 
-                            Diagnostics.Debug.Print(Simevent.Item("description"))
+                                Diagnostics.Debug.Print(Simevent.Item("description"))
 
-                            Using cmd1 As MySqlCommand = New MySqlCommand(stm, osconnection)
+                                Using cmd1 As MySqlCommand = New MySqlCommand(stm, osconnection)
 
-                                cmd1.Parameters.AddWithValue("@owneruuid", Simevent.Item("owneruuid"))
-                                cmd1.Parameters.AddWithValue("@name", Simevent.Item("name"))
-                                cmd1.Parameters.AddWithValue("@simname", Simevent.Item("simname"))
-                                cmd1.Parameters.AddWithValue("@category", Simevent.Item("category"))
-                                cmd1.Parameters.AddWithValue("@creatoruuid", Simevent.Item("creatoruuid"))
-                                cmd1.Parameters.AddWithValue("@description", Simevent.Item("description"))
-                                cmd1.Parameters.AddWithValue("@dateUTC", Simevent.Item("dateUTC"))
-                                cmd1.Parameters.AddWithValue("@duration", Simevent.Item("duration"))
-                                cmd1.Parameters.AddWithValue("@covercharge", Simevent.Item("covercharge"))
-                                cmd1.Parameters.AddWithValue("@coveramount", Simevent.Item("coveramount"))
-                                cmd1.Parameters.AddWithValue("@parcelUUID", Simevent.Item("parcelUUID"))
-                                cmd1.Parameters.AddWithValue("@globalPos", Simevent.Item("globalPos"))
-                                cmd1.Parameters.AddWithValue("@gateway", Simevent.Item("gateway"))
-                                cmd1.Parameters.AddWithValue("@eventflags", Simevent.Item("eventflags"))
+                                    cmd1.Parameters.AddWithValue("@owneruuid", Simevent.Item("owneruuid"))
+                                    cmd1.Parameters.AddWithValue("@name", Simevent.Item("name"))
+                                    cmd1.Parameters.AddWithValue("@simname", Simevent.Item("simname"))
+                                    cmd1.Parameters.AddWithValue("@category", Simevent.Item("category"))
+                                    cmd1.Parameters.AddWithValue("@creatoruuid", Simevent.Item("creatoruuid"))
+                                    cmd1.Parameters.AddWithValue("@description", Simevent.Item("description"))
+                                    cmd1.Parameters.AddWithValue("@dateUTC", Simevent.Item("dateUTC"))
+                                    cmd1.Parameters.AddWithValue("@duration", Simevent.Item("duration"))
+                                    cmd1.Parameters.AddWithValue("@covercharge", Simevent.Item("covercharge"))
+                                    cmd1.Parameters.AddWithValue("@coveramount", Simevent.Item("coveramount"))
+                                    cmd1.Parameters.AddWithValue("@parcelUUID", Simevent.Item("parcelUUID"))
+                                    cmd1.Parameters.AddWithValue("@globalPos", Simevent.Item("globalPos"))
+                                    cmd1.Parameters.AddWithValue("@gateway", Simevent.Item("gateway"))
+                                    cmd1.Parameters.AddWithValue("@eventflags", Simevent.Item("eventflags"))
 
-                                cmd1.BeginExecuteNonQuery()
-                                Application.DoEvents()
-                            End Using ' command
-                        End While
-                    End Using ' reader
-                End Using ' client
-
+                                    cmd1.BeginExecuteNonQuery()
+                                    Application.DoEvents()
+                                End Using ' command
+                            End While
+                        End Using ' reader
+                    End Using ' client
+                Catch
+                End Try
             End Using ' connection
         Catch ex As Exception
 
