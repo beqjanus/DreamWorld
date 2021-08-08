5df543992082162fa8b3b8a88dfcd94ff8b32e5e
Fred Beckhusen <fred.b@mitsi.com>
2021-08-07 22:39:29 -0500

    spelling

--- Installer_Src/Setup DreamWorld/Sorter.vb
+++ Installer_Src/Setup DreamWorld/Sorter.vb
@@ -42,7 +42,7 @@ Public Class ListViewColumnSorter
     ''' </summary>
     ''' <param name="x"></param>
     ''' <param name="y"></param>
-    ''' <returns> 1 if >, -1 if <, 0 if the ssame </returns>
+    ''' <returns> 1 if >, -1 if less than 0, 0 if the same </returns>
     Public Function Compare(x As Object, y As Object) As Integer
 
         If x Is Nothing Then Return 0
