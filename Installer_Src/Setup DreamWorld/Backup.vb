83b75fe840a9a6993a78fa6cc908bbc960d33132
Fred Beckhusen <fred.b@mitsi.com>
2021-09-02 06:34:25 -0500

    Drop extra space and enter on load oar

--- Installer_Src/Setup DreamWorld/Backup.vb
+++ Installer_Src/Setup DreamWorld/Backup.vb
@@ -27,7 +27,7 @@
 
             ConsoleCommand(RegionUUID, "change region " & """" & PropRegionClass.RegionName(RegionUUID) & """")
             ConsoleCommand(RegionUUID, "save oar  " & """" & BackupPath() & "/" & PropRegionClass.RegionName(RegionUUID) & "_" &
-                               DateTime.Now.ToString("yyyy-MM-dd_HH_mm_ss", Globalization.CultureInfo.InvariantCulture) & ".oar {ENTER}" & """")
+                               DateTime.Now.ToString("yyyy-MM-dd_HH_mm_ss", Globalization.CultureInfo.InvariantCulture) & ".oar" & """")
         Next
 
     End Sub
