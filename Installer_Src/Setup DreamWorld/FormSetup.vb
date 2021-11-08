990ae0016f95e6b71af7e9bcaed263519fde961a
Fred Beckhusen <fred@outworldz.com>
2021-11-07 18:57:21 -0600

    V4.76
    Signed-off-by: Fred Beckhusen <fred@outworldz.com>

--- Installer_Src/Setup DreamWorld/FormSetup.vb
+++ Installer_Src/Setup DreamWorld/FormSetup.vb
@@ -1594,7 +1594,7 @@ Public Class FormSetup
         Dim v = Reflection.Assembly.GetExecutingAssembly().GetName().Version
         Dim buildDate = New DateTime(2000, 1, 1).AddDays(v.Build).AddSeconds(v.Revision * 2)
         Dim displayableVersion = $"{v} ({buildDate})"
-        AssemblyV = "Assembly version:" + displayableVersion
+        AssemblyV = "Assembly version: " + displayableVersion
         TextPrint(AssemblyV)
 
         SetupPerl()
