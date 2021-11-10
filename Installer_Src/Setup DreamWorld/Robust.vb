0f784991504440a0a4edcb08947ad74d7bf20eb7
Fred Beckhusen <fred@outworldz.com>
2021-11-09 22:26:38 -0600

    Set SmartStart settings in Robust after Ubit reorged it

--- Installer_Src/Setup DreamWorld/Robust.vb
+++ Installer_Src/Setup DreamWorld/Robust.vb
@@ -395,7 +395,7 @@ Module Robust
         INI.SetIni("AssetService", "SpoolDirectory", Settings.BaseDirectory & "/tmp")
         INI.SetIni("AssetService", "ShowConsoleStats", Settings.ShowConsoleStats)
 
-        INI.SetIni("SmartStart", "Enabled", CStr(Settings.SmartStart))  ' FKB
+
         INI.SetIni("ServiceList", "GetTextureConnector", """" & "${Const|PublicPort}/Opensim.Capabilities.Handlers.dll:GetTextureServerConnector" & """")
 
         If Settings.CMS = JOpensim Then
@@ -417,6 +417,18 @@ Module Robust
         End If
 
         INI.SetIni("DatabaseService", "ConnectionString", Settings.RobustDBConnection)
+
+        ' SmartStart. Add own entries for DreamGrid host and port
+        ' In future that may need to be more clever, as per machine in a servers cluster
+        If Settings.SmartStart Then
+            INI.SetIni("SmartStart", "Enabled", "True")
+            INI.SetIni("SmartStart", "URL", "http://" & Settings.LANIP() + ":" & CStr(Settings.DiagnosticPort))
+            INI.SetIni("SmartStart", "MachineID", CStr(Settings.MachineID))
+        Else
+            INI.SetIni("SmartStart", "Enabled", "False")
+            INI.SetIni("SmartStart", "URL", "")
+            INI.SetIni("SmartStart", "MachineID", "")
+        End If
 
         INI.SaveINI()
 
