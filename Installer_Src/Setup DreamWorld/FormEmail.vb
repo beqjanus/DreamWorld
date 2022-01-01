47ffb3f59f88f487aa6b66c75abc60413a29ecb5
Fred Beckhusen <fred@outworldz.com>
2021-12-31 21:46:56 -0600

    Add save pos to FormEmail

--- Installer_Src/Setup DreamWorld/FormEmail.vb
+++ Installer_Src/Setup DreamWorld/FormEmail.vb
@@ -4,11 +4,43 @@ Imports MimeKit
 
 Public Class FormEmail
 
-    Private ReadOnly Contacts As New Dictionary(Of String, String)
+    Private ReadOnly Contacts As New Dictionary(Of String, String
+        )
+
+#Region "ScreenSize"
+
+    Private ReadOnly Handler As New EventHandler(AddressOf Resize_page)
+    Private _screenPosition As ClassScreenpos
+
+    Public Property ScreenPosition As ClassScreenpos
+        Get
+            Return _screenPosition
+        End Get
+        Set(value As ClassScreenpos)
+            _screenPosition = value
+        End Set
+    End Property
+
+    'The following detects  the location of the form in screen coordinates
+    Private Sub Resize_page(ByVal sender As Object, ByVal e As System.EventArgs)
+        'Me.Text = "Form screen position = " + Me.Location.ToString
+        ScreenPosition.SaveXY(Me.Left, Me.Top)
+    End Sub
+
+    Private Sub SetScreen()
+
+        ScreenPosition = New ClassScreenpos(Me.Name)
+        AddHandler ResizeEnd, Handler
+        Dim xy As List(Of Integer) = ScreenPosition.GetXY()
+        Me.Left = xy.Item(0)
+        Me.Top = xy.Item(1)
+    End Sub
+
+#End Region
 
     Public Sub Init(L As ListView)
         If L Is Nothing Then Return
-
+        SetScreen()
         If Settings.SmtPropUserName = "LoginName@gmail.com" Then
             MsgBox(My.Resources.No_Email, vbInformation Or MsgBoxStyle.MsgBoxSetForeground, "Oops")
 #Disable Warning CA2000
@@ -91,7 +123,6 @@ Public Class FormEmail
 
             End Using
         End Using
-
 
         Me.Close()
     End Sub
