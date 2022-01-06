780c5cb5908c720cd6406abcb3f35c320b6748e8
Fred Beckhusen <fred@outworldz.com>
2022-01-05 19:04:46 -0600

    Clean up B&W pages

--- OutworldzFiles/Apache/htdocs/DreamGrid/index.php
+++ OutworldzFiles/Apache/htdocs/DreamGrid/index.php
@@ -1,27 +1,20 @@
-<html>
+﻿<html>
  <head>
-  <title>Welcome to Dreamworld</title>
-  <link rel="shortcut icon" href="/favicon.ico"
- </head>
- <body>
-
+  <title>Welcome to DreamGrid</title>
+  <link rel="shortcut icon" href="/favicon.ico">
+</head>
+<body>
 <?php
   include("../Metromap/includes/config.php");
 ?>
 
-
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">        
-        <title>Dreamgrid Opensimulator by Outworldz.com</title>
-        <link rel="shortcut icon" href="favicon.ico">
-    </head>
-    <body>
 [ <a href="/Search">Search</a>  | 
 Log in to <a href="http://<?php echo $CONF_domain.":".$CONF_port ?>">DreamGrid</a> |
 <a href="/Stats">Visitor Stats</a> |
 <a href="http://<?php echo $CONF_domain.":".$CONF_port ?>/wifi/map.html">Map</a> ] 
+<p>
 <iframe frameborder="0" height="900" width="1600" src="/Metromap/index.php"></iframe>
+</p>
 <a rel="license" href="https://www.outworldz.com">Dreamgrid by Outworldz.com is licensed AGPL 3.0</a>
 <p>
  DreamGrid is a <a href="https://www.outworldz.com/Outworldz_installer/">free Opensimulator server</a> powered by <a href="https://www.Outworldz.com">Outworldz.com</a>
