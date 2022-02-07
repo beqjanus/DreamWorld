d5cd4c7ce8209425bbfe35f8ea8cc8d76a8cc406
Fred Beckhusen <fred@outworldz.com>
2022-02-06 20:51:50 -0600

    Signed-off-by: Fred Beckhusen <fred@outworldz.com>

--- OutworldzFiles/Apache/htdocs/MetroMap/includes/config.php
+++ OutworldzFiles/Apache/htdocs/MetroMap/includes/config.php
@@ -11,8 +11,8 @@ $CONF_db_user       = "robustuser";  // login
 $CONF_db_pass       = "robustpassword";  // password 
 $CONF_db_database   = "Robust";     // Name Of Robust Server 
 /* The Coordinates Of the Grid-Center */ 
-$CONF_center_coord_x = "1017";		// the Center-X-Coordinate 
-$CONF_center_coord_y = "1001";		// the Center-Y-Coordinate 
+$CONF_center_coord_x = "1003";		// the Center-X-Coordinate 
+$CONF_center_coord_y = "994";		// the Center-Y-Coordinate 
 // style-sheet items
 $CONF_style_sheet     = "/css/stylesheet.css";          //Link To your StyleSheet
 $CONF_HOME            = "DreamGrid";          //Link To your Home Folder in htdocs.  WordPress, DreamGrid, JOpensim/jOpensim or user assigned folder
