5e7a89e082be1c41b6d30a369fad0109754a84e3
Fred Beckhusen <fred@outworldz.com>
2022-05-03 22:43:38 -0500

    V 4.996

--- Installer_Src/Setup DreamWorld/GlobalSettings.vb
+++ Installer_Src/Setup DreamWorld/GlobalSettings.vb
@@ -11,8 +11,8 @@ Module GlobalSettings
 
     Public Const _Domain As String = "http://www.outworldz.com"
     Public Const _httpsDomain As String = "https://www.outworldz.com"
-    Public Const _MyVersion As String = "4.996"
-    Public Const _SimVersion As String = "Opensim 0.9.2.2 #b7ed5f109ec9b28 prebuild for .net6, disable ..."
+    Public Const _MyVersion As String = "4.998"
+    Public Const _SimVersion As String = "Opensim 0.9.2.2 #4f7b293c5efe add commandline option excluddir to Prebuild ..."
     Public Const FreeDiskSpaceWarn As Long = 100000000
     Public Const Hyperica As String = "Hyperica"
     Public Const JOpensim As String = "JOpensim"
