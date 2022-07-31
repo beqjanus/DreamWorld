1e23ff64cef7d0ebd0222558e3841bb5f7046ea8
Fred Beckhusen <fred@outworldz.com>
2022-07-31 07:43:06 -0500

    No idea, no changes

--- Installer_Src/Setup DreamWorld/OfflineIM.vb
+++ Installer_Src/Setup DreamWorld/OfflineIM.vb
@@ -55,13 +55,13 @@ Module OfflineIM
                 If Settings.EmailEnabled And Settings.EnableEmailToSMTPCheckBox Then
                     Dim Mails = OfflineEmails()
                     For Each email In Mails
-                        Dim FromPerson = AvatarEmailData(email.fromid)
-                        Dim ToPerson = AvatarEmailData(email.principalid)
+                        Dim FromPerson = AvatarEmailData(email.Fromid)
+                        Dim ToPerson = AvatarEmailData(email.Principalid)
                         If ToPerson.Email.Length > 0 Then
                             '<?xml version="1.0" encoding="utf-8"?><GridInstantMessage xmlns:xsd = "http://www.w3.org/2001/XMLSchema" xmlns:xsi = "http://www.w3.org/2001/XMLSchema-instance" <> fromAgentID > fa378d99 - c4d5 - 4cc0-93E1-92E70ca49355</fromAgentID><fromAgentName>test user</fromAgentName><toAgentID>ed8813ef-361d-4005-8Fdd-a1f89908300e</toAgentID><dialog>0</dialog><fromGroup>False</fromGroup><message>test 2</message><imSessionID>17bf9e76-f2c8-0cc5-1c3c-331F95aca35b</imSessionID><offline>0</offline><Position><X>0</X><Y>0</Y><Z>0</Z></Position><binaryBucket>AA==</binaryBucket><ParentEstateID>1</ParentEstateID><RegionID>33aeeb96-c012-41E4-8edd-d78aa22e6444</RegionID><timestamp>1657327580</timestamp></GridInstantMessage>
 
                             Dim pattern1 = New Regex("<message>(.*?)</message>")
-                            Dim match1 As Match = pattern1.Match(email.message)
+                            Dim match1 As Match = pattern1.Match(email.Message)
                             Dim msg As String = ""
                             If match1.Success Then
                                 msg = match1.Groups(1).Value
@@ -74,7 +74,7 @@ Module OfflineIM
                             Dim result = SendEmail($"{FromPerson.FirstName} {FromPerson.LastName}", $"{ToPerson.FirstName} {ToPerson.LastName}", $"IM from {FromPerson.FirstName} {FromPerson.LastName}", msg)
                             If result = My.Resources.Ok Then
                                 Logger("Email", $"Offline IM Email sent from {FromPerson.FirstName} {FromPerson.LastName} to {ToPerson.FirstName} {ToPerson.LastName}", "Outworldz")
-                                DeleteIM(email.id)
+                                DeleteIM(email.Id)
                             End If
                         End If
                     Next
