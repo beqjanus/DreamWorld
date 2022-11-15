a112a2a2a92bfa355962cc4977a9c90c8316fee0
Fred Beckhusen <fred@outworldz.com>
2022-11-15 14:17:58 +0100

    Add column grid, drop unique on Avatar name to TOS DB

--- OutworldzFiles/Mysql/bin/Create_TOS.sql
+++ OutworldzFiles/Mysql/bin/Create_TOS.sql
@@ -1,12 +1,13 @@
 use robust;
 CREATE TABLE IF NOT EXISTS tosauth (  
-  avataruuid VARCHAR(36) NOT NULL, 
+  avataruuid VARCHAR(36) NOT NULL,   
   agreed INT NOT NULL DEFAULT 0,
   avatarname VARCHAR(64),
+  grid VARCHAR(64) NOT NULL,
   createtime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   confirmtime DATETIME NULL DEFAULT  CURRENT_TIMESTAMP,
   token VARCHAR(36) NOT NULL,
-  UNIQUE INDEX uuid_UNIQUE (avataruuid ASC),
-  UNIQUE INDEX avatarname_UNIQUE (avatarname ASC)
+  
+  UNIQUE INDEX uuid_UNIQUE (avataruuid ASC)  
 );
 
