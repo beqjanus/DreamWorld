47b8a991f7adb347572d0f01a40ba3002497b37e
Fred Beckhusen <fred@outworldz.com>
2022-12-29 23:51:27 +0100

    Apache redirect fixes

--- OutworldzFiles/Apache/htdocs/index.php
+++ OutworldzFiles/Apache/htdocs/index.php
@@ -1,5 +1,5 @@
 <?php
 	include("./Metromap/includes/config.php");
 	//header("location: $CONF_HOME\n\n", true, 302);
-	echo ("<meta http-equiv=\"refresh\" content=\"1,url='$CONF_HOME'\">");
+	echo ("<meta http-equiv=\"refresh\" content=\"1,url='/$CONF_HOME'\">");
 ?>
